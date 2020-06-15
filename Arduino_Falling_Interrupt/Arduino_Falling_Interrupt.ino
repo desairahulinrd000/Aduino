793cebf9b5d188b2db0e10f9deda04b880460359
desairahulinrd000 <56038822+desairahulinrd000@users.noreply.github.com>
2020-06-16 00:26:45 +0530

    Add files via upload

---
+++ Arduino_Falling_Interrupt/Arduino_Falling_Interrupt.ino
@@ -0,0 +1,23 @@
+const int ledPin = 12;
+const int int_led_pin=10;
+const int interruptPin = 2;
+volatile byte state = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+   pinMode(10, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, FALLING);
+}
+
+void loop() {
+  digitalWrite(interruptPin, state);
+  digitalWrite(ledPin,HIGH);
+  delay(10000);
+  digitalWrite(ledPin,LOW);
+  delay(10000);
+}
+
+void blink() {
+  state = !state;
+}
